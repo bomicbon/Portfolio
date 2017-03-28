bb736e2abfb5bedf606b1c6543c0e339deae74f1
bomicbon <urianlee@yahoo.com>
2017-03-27 22:49:13 -0700

    particle system in full effect bitches

--- src/sketch.js
+++ src/sketch.js
@@ -1,14 +1,14 @@
 var system;
 var canvas;
 function setup() {
-	canvas = createCanvas(windowWidth, windowHeight / 2);
+	canvas = createCanvas(windowWidth, windowHeight);
 	canvas.parent('sketch-holder');
-	system = new ParticleSystem(createVector(width/2, 50));
-	for (i=0; i<20; i++) {
+	system = new ParticleSystem(createVector(width/2, 100));
+	for (i=0; i<500; i++) {
 		system.addParticle();
 	}
 }
-
+  
 function draw() {
 	background("#fff");
 	//system.addParticle();
@@ -18,7 +18,7 @@ function draw() {
 // A simple Particle class
 var Particle = function(position) {
 	var xacc = random(-0.05, 0.05);
-	var yacc = random(-0.05, 0.05);
+	var yacc = random(0, 0.05);
 	this.acceleration = createVector(xacc, yacc);
 	this.velocity = createVector(random(-1, 1), random(-1, 0));
 	this.position = position.copy();
@@ -35,26 +35,29 @@ Particle.prototype.update = function(){
 	this.velocity.add(this.acceleration);
 	this.position.add(this.velocity);
 	if (this.position.x < 0 || this.position.x > windowWidth) {
-		this.acceleration.x *= -1;
-		this.acceleration.y *= -1;
-		this.velocity.x *= -1;
-		this.velocity.y *= -1;
+		this.reverse();
 	}
-	if (this.position.y < 0 || this.position.y > windowHeight /2) {
-		this.acceleration.x *= -1;
-		this.acceleration.y *= -1;
-		this.velocity.x *= -1;
-		this.velocity.y *= -1;
+	else if (this.position.y < 0 || this.position.y > windowHeight) {
+		this.reverse();
 	}
 	//this.lifespan -= 2;
-}
+};
+
+Particle.prototype.reverse = function() {
+	var drag = -1;
+	//this.acceleration.x *= drag;
+	//this.acceleration.y *= drag;
+	this.velocity.x *= drag;
+	this.velocity.y *= drag;
+};
 
 // Method to display
 Particle.prototype.display = function() {
 	stroke(200, this.lifespan);
 	strokeWeight(2);
 	fill(127, this.lifespan);
-	ellipse(this.position.x, this.position.y, 12, 12);
+	//ellipse(this.position.x, this.position.y, 12, 12);
+	rect(this.position.x, this.position.y, 12, 12);
 };
 
 // Is the particle still useful?
